519a7cfc4ef004de7f8747b6df3174017e554d54
Rinkal Panda <153069622+Rinkal0505@users.noreply.github.com>
2023-12-19 21:47:41 +0530

    Add files via upload

---
+++ MY BLOG/config.php
@@ -0,0 +1,7 @@
+/* phpMyAdmin configuration snippet */
+
+/* Paste it to your config.inc.php */
+
+$cfg['Console']['Mode'] = 'show';
+$cfg['Server']['hide_db'] = '';
+$cfg['Server']['only_db'] = '';
